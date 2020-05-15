372d2a3566c3ce6b42076d2a71eb115c3e681742
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-15 13:31:51 +0400

    fix

--- User.js
+++ User.js
@@ -1,23 +1,25 @@
-window.User = {
-	is:function(){
-		var user=this.get();
-		return user.is;
-	},
-	get:function(){
-		var json='-user/get.php';
-		Global.unload('user',json)
-		return Load.loadJSON(json);
-	},
-	isAdmin:function(){
-		var user = this.get();
-		return user.admin;
-	},
-	getEmail:function(){
-		var user  =this.get();
-		return user.email;
-	},
-	lang: function (str){
-		if(typeof(str) == 'undefined') return Lang.name('user');
-		return Lang.str('user', str);
-	}
+let User = {
+    is: function() {
+        var user = this.get();
+        return user.is;
+    },
+    get: function() {
+        var json = '-user/get.php';
+        Global.unload('user', json)
+        return Load.loadJSON(json);
+    },
+    isAdmin: function() {
+        var user = this.get();
+        return user.admin;
+    },
+    getEmail: function() {
+        var user = this.get();
+        return user.email;
+    },
+    lang: function(str) {
+        if (typeof(str) == 'undefined') return Lang.name('user');
+        return Lang.str('user', str);
+    }
 }
+window.User = User
+export { User }
--- infra.js
+++ infra.js
@@ -1,7 +1,8 @@
 
-Event.one('Controller.oninit', function () {
-	Template.scope['User'] = {};
-	Template.scope['User']['lang'] = function (str) {
-		return User.lang(str);
-	};
-});
+import { Template } from '/vendor/infrajs/template/Template.js'
+import { User} from '/vendor/infrajs/user/User.js'
+
+Template.scope['User'] = {}
+Template.scope['User']['lang'] = function (str) {
+	return User.lang(str)
+}
